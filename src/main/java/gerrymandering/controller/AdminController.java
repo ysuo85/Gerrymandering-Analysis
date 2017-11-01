e701b0fda13ff8e0e694d9869831b54e983ed576
ysuo85 <yi.suo@stonybrook.edu>
2017-11-01 13:47:35 -0400

    Removes uneeded code.

--- src/main/java/gerrymandering/controller/AdminController.java
+++ src/main/java/gerrymandering/controller/AdminController.java
@@ -1,13 +1,7 @@
-package gerrymandering;
+package gerrymandering.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.ServletRequest;
-import java.util.Map;
 
 /**
  * Created by yisuo on 10/30/17.
@@ -25,7 +19,7 @@ public class AdminController {
     }
 
     @RequestMapping("/login")
-    public String login(ServletRequest request){
+    public String login(){
         return "loginView";
     }
 }
