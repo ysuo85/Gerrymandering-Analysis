ce771591d8abbb38f31e2f94a00d42860ffa0e7b
ysuo85 <yi.suo@stonybrook.edu>
2017-11-01 20:54:40 -0400

    Adds authentication for admin.html.

--- src/main/java/gerrymandering/config/SecurityConfig.java
+++ src/main/java/gerrymandering/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+                .antMatchers("/www/admin.html").hasRole("ADMIN")
                 .antMatchers("/resources/**", "/www/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
