2da679ca4b3e053738992842056d2ccaa6a7d992
ysuo85 <yi.suo@stonybrook.edu>
2017-11-12 09:22:34 -0500

    Creates services.

---
+++ src/main/java/gerrymandering/service/DemographicService.java
@@ -0,0 +1,19 @@
+package gerrymandering.service;
+
+import gerrymandering.common.PopulationGroup;
+import gerrymandering.model.State;
+
+import java.util.List;
+
+/**
+ * Created by yisuo on 11/9/17.
+ */
+public class DemographicService {
+    public Integer getMinorityDensity(State state){
+        return 0;
+    }
+
+    public Integer getMinorityDensity(State state, List<PopulationGroup> ethnicGroups){
+        return 0;
+    }
+}
