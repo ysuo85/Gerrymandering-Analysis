34c45c02f33e22396c39ad97414a02e0d3cfb712
ysuo85 <yi.suo@stonybrook.edu>
2017-11-15 16:22:50 -0500

    Fixes spacing and typos in Python scripts.

--- dataParsers/censusParser.py
+++ dataParsers/censusParser.py
@@ -1,11 +1,9 @@
-
 input  = open("CensusData/Virginia.csv", 'r')
 output = open("parsedFiles/VirginiaCensus.csv", 'w')
 
-COMMMA  = ','
+COMMA  = ','
 NEWLINE = '\n'
 OFFSET  = 2
-
 
 for line in input:
     line = line.replace('\"', '')
@@ -13,26 +11,25 @@ for line in input:
     data = []
     newLine = ''
     if(line[0] == "People"):
-        if(line[2] == "Total population" and line[1] == "Sex and Age" and (line[1] != "Race"  and line[1] != "Hispanic or Latino and Race")):
+        if(line[2] == "Total population" and line[1] == "Sex and Age" and (line[1] != "Race" and \
+        line[1] != "Hispanic or Latino and Race")):
             data.append("Total population")
             for i in range(len(line)):
                 if i%2 != 0 and i + OFFSET < len(line):
                     data.append(line[i + OFFSET])
-
-        elif ((line[1].lower()== 'race' or (line[1] == "Hispanic or Latino and Race" and line[2] == "Hispanic or Latino (of any race)")) and line[2] != "Total population"):
-                if line[2].lower() != 'one race':
-                    data.append(line[2])
-
-                    for i in range(len(line)):
-                        if i % 2 != 0 and i + OFFSET < len(line):
-                            data.append(line[i + OFFSET])
+        elif ((line[1].lower()== 'race' or (line[1] == "Hispanic or Latino and Race" and \
+        line[2] == Hispanic or Latino (of any race)")) and line[2] != "Total population"):
+            if line[2].lower() != 'one race':
+                data.append(line[2])
+                for i in range(len(line)):
+                    if i % 2 != 0 and i + OFFSET < len(line):
+                        data.append(line[i + OFFSET])
 
         for i in range(len(data)):
             if i != len(data)-1:
-                newLine += data[i] + COMMMA
+                newLine += data[i] + COMMA
             else:
                 newLine += data[i] + NEWLINE
 
         if len(newLine) > 1:
             output.write(newLine)
-
--- dataParsers/kmlStateParser.py
+++ dataParsers/kmlStateParser.py
@@ -1,11 +1,9 @@
 input       = open('geoData/cb_2016_us_state_20m/cb_2016_us_state_20m.kml', 'r')
 output      = open('parsedFiles/StateGeo.csv', 'w')
 outputLine  = ''
-
 stateFp = None
 name    = ''
 cord    = ''
-
 
 for line in input:
     if "<SimpleData name=\"STATEFP\">" in line:
@@ -13,21 +11,17 @@ for line in input:
             outputLine = outputLine[:-1]
             outputLine += '\n'
             output.write(outputLine)
-
         stateFp     = line.replace("<SimpleData name=\"STATEFP\">", '')
         stateFp     = stateFp.replace("</SimpleData>\n", '')
         outputLine  = stateFp + ';'
-
     elif "<SimpleData name=\"NAME\">" in line:
         name        = line.replace("<SimpleData name=\"NAME\">", '')
         name        = name.replace("</SimpleData>\n", '')
         outputLine += name + ';'
-
     elif "<coordinates>" in line:
         cord        = line.replace("<coordinates>", '')
         cord        = cord.replace("</coordinates>\n", '')
         outputLine += '(' + cord + ')' + ';'
-
 
 input.close()
 output.close()
